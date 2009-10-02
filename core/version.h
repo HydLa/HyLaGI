3a421b6fb239a738a3a66abb2285215421103196
hirose <hirose@db2ee5a7-f324-0410-8b40-dae30602bd36>
2009-10-02 04:49:14 +0000

    日付等の表示修正

--- core/version.h
+++ core/version.h
@@ -2,6 +2,8 @@
 #define _INCLUDED_HYDLA_VERSION_H_
 
 #include <sstream>
+#include <string>
+#include <algorithm>
 
 /*
  * To enable subversion propset
@@ -18,12 +20,14 @@ class Version {
     return "0.2.5";
   }
   
-  static const char* revision() {
-    return "$Revision$"; //template for subversion
+  static std::string revision() {
+    std::string rev("$Revision$"); //template for subversion
+    return rev.substr(11, rev.size() - 11 - 2);
   }
   
-  static const char* last_change_date() {
-    return "$LastChangedDate$"; //template for subversion
+  static std::string last_change_date() {
+    std::string date("$LastChangedDate$"); //template for subversion
+    return date.substr(18, date.find(' ', 18)-18);
   }
 
   static const char* copyright() {
