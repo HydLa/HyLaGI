227f9a1d8e993c353a7f015117f485d3d55d2121
kenshiro <kenshiro@db2ee5a7-f324-0410-8b40-dae30602bd36>
2009-12-10 12:37:16 +0000

    is_consistent function

--- branch_and_prune_simulator/ConsistencyChecker.cpp
+++ branch_and_prune_simulator/ConsistencyChecker.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <cassert>
 
+#include "realpaver.h"
+
 using namespace hydla::parse_tree;
 using namespace hydla::simulator;
 
@@ -57,33 +59,67 @@ bool ConsistencyChecker::is_consistent(TellCollector::tells_t& collected_tells)
   }
   rp_vector_destroy(&vec);
 
-  //// 問題とソルバを作成し,解いてチェック
-  //rp_problem problem;
-  //rp_problem_create(&problem, "consistency_check");
-  //// 変数ベクタに変数を追加
-  //int size = this->vars_.size();
-  //for(int i=0; i<size; i++){
-  //  rp_variable v;
-  //  rp_variable_create(&v, ((this->vars_.right.at(i)).c_str()));
-  //  // TODO: すべての変数は初期値[-oo,+oo]をもつ,ここで入れるとよいかも
-  //  rp_vector_insert(rp_problem_vars(problem), v);
-  //}
-  //// TODO: 制約ベクタに制約を追加
+  // 問題とソルバを作成し,解いてチェック
+  rp_problem problem;
+  rp_problem_create(&problem, "consistency_check");
 
-  //// 変数の初期値からボックスが自動作成される
-  //rp_problem_set_initial_box(problem);
+  // 変数ベクタに変数を追加
+  int size = this->vars_.size();
+  for(int i=0; i<size; i++){
+    rp_variable v;
+    rp_variable_create(&v, ((this->vars_.right.at(i)).c_str()));
+    // TODO: すべての変数は初期値[-oo,+oo]をもつ,ここで入れるとよいかも
+    rp_interval i;
+    rp_interval_set(i,(-1)*RP_INFINITY,RP_INFINITY);
+    rp_union_insert(rp_variable_domain(v), i);
+    rp_vector_insert(rp_problem_vars(problem), v);
+  }
 
-  //// TODO: ソルバを作成して求解
+  // TODO: 制約ベクタに制約を追加
+  std::set<rp_constraint>::iterator it = this->constraints_.begin();
+  while(it != this->constraints_.end())
+  {
+    rp_vector_insert(rp_problem_ctrs(problem), *it);
+    for(int i=0; i<rp_constraint_arity(*it); i++)
+    {
+      ++rp_variable_constrained(rp_problem_var(problem, rp_constraint_var(*it, i)));
+    }
+    it++;
+  }
+
+  // 変数の初期値からボックスが自動作成される
+  rp_problem_set_initial_box(problem);
+
+  // TODO: ソルバを作成して求解
+  rp_selector * select;
+  //rp_new(select,rp_selector_decirdom,(&problem));
+  //rp_new(select,rp_selector_decirrobust,(&problem,1));
+  rp_new(select,rp_selector_roundrobin,(&problem));
+
+  rp_splitter * split;
+  rp_new(split,rp_splitter_mixed,(&problem));
+  //rp_new(split,rp_splitter_bisection,(&problem));
+
+  //rp_interval_satisfaction_prover * prover;
+  //rp_new(prover,rp_interval_satisfaction_prover,(&problem,100000));
+
+  rp_bpsolver solver(&problem,10,select,split); //,prover);
+
+  rp_problem_display(stdout,problem);
+  std::cout << "\n";
+
+  rp_box sol;
+  sol = solver.compute_next();
 
   // 後始末
-  std::set<rp_constraint>::iterator it = this->constraints_.begin();
+  it = this->constraints_.begin();
   while(it != this->constraints_.end()){
     rp_constraint_destroy(((rp_constraint *)&(*it)));
     this->constraints_.erase(it++);
   }
 
   // return ソルバから解が一つでも出力させたか？
-  return true;
+  return (sol != NULL);
 }
 
 /**
