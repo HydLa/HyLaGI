f0a128947e30a5a55dc9e8372c3e77d3f8036661
hirose <hirose@db2ee5a7-f324-0410-8b40-dae30602bd36>
2010-01-15 13:04:12 +0000

    integrate resultの構築処理

--- symbolic_simulator/Integrator.cpp
+++ symbolic_simulator/Integrator.cpp
@@ -107,6 +107,9 @@ IntegrateResult Integrator::integrate(
   ml_.skip_pkt_until(RETURNPKT);
 
   HYDLA_LOGGER_DEBUG("---integrate calc result---");
+  IntegrateResult integrate_result;
+  integrate_result.states.resize(1);
+  NextPointPhaseState& state = integrate_result.states.back();
 
   ml_.MLGetNext(); // List関数
   int list_size = ml_.get_arg_count();
@@ -114,153 +117,83 @@ IntegrateResult Integrator::integrate(
   
   // List[制約一覧, 変化したaskとそのIDの組の一覧]が返る
   ml_.MLGetNext(); // Listという関数名
+  ml_.MLGetNext(); // Listの中の先頭要素
 
   // next_point_phase_timeを得る
-  ml_.MLGetNext(); // Listの中の先頭要素
   SymbolicTime next_point_phase_time;
-  next_point_phase_time.str = ml_.get_string();
-  HYDLA_LOGGER_DEBUG("next_point_phase_time : ", next_point_phase_time.str);  
-
-  // 制約一覧を得る
+  state.next_point_phase_time.str = ml_.get_string();
+  HYDLA_LOGGER_DEBUG("next_point_phase_time : ", state.next_point_phase_time.str);  
   ml_.MLGetNext(); // Listという関数名
-  // List関数の要素数（制約の個数）を得る
+  
+  // 変数表の作成
   int variable_list_size = ml_.get_arg_count();
   HYDLA_LOGGER_DEBUG("variable_list_size : ", variable_list_size);  
-  ml_.MLGetNext(); // List中の先頭要素
-
-  variable_map_t variable_map;
+  ml_.MLGetNext(); ml_.MLGetNext();
   for(int i=0; i<variable_list_size; i++)
   {
     ml_.MLGetNext(); ml_.MLGetNext();
-    
-    std::string name = ml_.get_symbol();
-    int derivative = ml_.get_integer();
-    std::string value = ml_.get_string();
 
-    HYDLA_LOGGER_DEBUG("name  : ", name);
+    SymbolicVariable variable;
+    SymbolicValue    value;
 
-//    std::string variable_value = ml_.get_string();
+    HYDLA_LOGGER_DEBUG("--- add variable ---");
 
-//     HYDLA_LOGGER_DEBUG(
-//       "name  : ", variable_name,
-//       "value : ", variable_value);
+    // 変数名
+    variable.name = ml_.get_symbol();
+    HYDLA_LOGGER_DEBUG("name  : ", variable.name);
+    ml_.MLGetNext();
 
-//    tmp_cons.insert(str);
+    // 微分回数
+    variable.derivative_count = ml_.get_integer();
+    HYDLA_LOGGER_DEBUG("derivative : ", variable.derivative_count);
+    ml_.MLGetNext();
+
+    // 値
+    value.str = ml_.get_string();
+    HYDLA_LOGGER_DEBUG("value : ", value.str);
+    ml_.MLGetNext();
+
+    state.variable_map.set_variable(variable, value); 
   }
-  ml_.MLGetNext(); // List関数
-/*
 
   // askとそのIDの組一覧を得る
-  int changed_asks_size;
-  if(! ml_.MLGetArgCount(&changed_asks_size)){
-    std::cout << "#funcCase:MLGetArgCount:unable to get the number of arguments from ml" << std::endl;
-    throw MathLinkError("MLGetArgCount", ml_.MLError());
-  }
-  bool is_max_time = changed_asks_size > 0; // 変化したaskがない場合はmax_timeに達した場合である
+  int changed_asks_size = ml_.get_arg_count();
+  HYDLA_LOGGER_DEBUG("changed_asks_size : ", changed_asks_size);
+  state.is_max_time = changed_asks_size > 0; // 変化したaskがない場合はmax_timeに達した場合である
+  HYDLA_LOGGER_DEBUG("is_max_time : ", state.is_max_time);
+  ml_.MLGetNext(); // List関数
   ml_.MLGetNext(); // Listという関数名
-  ask_list_t changed_asks;
   for(int j=0; j<changed_asks_size; j++)
   {
+    HYDLA_LOGGER_DEBUG("--- add changed ask ---");
+
     ml_.MLGetNext(); // List関数
     ml_.MLGetNext(); // Listという関数名
     std::string changed_ask_type_str = ml_.get_symbol(); // pos2negまたはneg2pos
+    HYDLA_LOGGER_DEBUG("changed_ask_type_str : ", changed_ask_type_str);
+
     AskState changed_ask_type;
     if(changed_ask_type_str == "pos2neg"){
       changed_ask_type = Positive2Negative;
     }
-    else{
+    else if(changed_ask_type_str == "neg2pos") {
       changed_ask_type = Negative2Positive;
     }
-    int changed_ask_id;
-    if(! ml_.MLGetInteger(&changed_ask_id)){
-      HYDLA_LOGGER_ERROR("MLGetInteger:unable to read the int from ml");
-      throw MathLinkError("MLGetInteger", ml_.MLError());
+    else {
+      assert(0);
     }
-    changed_asks.push_back(std::make_pair(changed_ask_type, changed_ask_id));
+
+    int changed_ask_id = ml_.get_integer();
+    HYDLA_LOGGER_DEBUG("changed_ask_id : ", changed_ask_id);
+
+    integrate_result.ask_list.push_back(
+      std::make_pair(changed_ask_type, changed_ask_id));
   }
 
+  HYDLA_LOGGER_DEBUG(
+    "--- integrate result ---\n", 
+    integrate_result);
 
-  // tmp_consから変数表をつくる
-  std::set<std::string>::iterator tmp_cons_it = tmp_cons.begin();
-  variable_map_t variable_map;
-  while((tmp_cons_it) != tmp_cons.end())
-  {
-    std::string cons_str = (*tmp_cons_it);
-    // cons_strは"Equal[usrVarx,2]"や"Equal[Derivative[1][usrVary],3]"など
-
-    unsigned int loc = cons_str.find("Equal[", 0);
-    loc += 6; // 文字列"Equal["の長さ分
-    unsigned int comma_loc = cons_str.find(",", loc);
-    if(comma_loc == std::string::npos)
-    {
-      std::cout << "can't find comma." << std::endl;
-//      return;
-    }
-    std::string variable_str = cons_str.substr(loc, comma_loc-loc);
-    // variable_strは"usrVarx"や"Derivative[1][usrVarx]"など
-
-    // nameとderivative_countへの分離
-    std::string variable_name;
-    int variable_derivative_count;
-    unsigned int variable_loc = variable_str.find("Derivative[", 0);
-    if(variable_loc != std::string::npos)
-    {
-      variable_loc += 11; // "Derivative["の長さ分
-      unsigned int bracket_loc = variable_str.find("][", variable_loc);
-      if(bracket_loc == std::string::npos)
-      {
-        std::cout << "can't find bracket." << std::endl;
-//        return;
-      }
-      std::string variable_derivative_count_str = variable_str.substr(variable_loc, bracket_loc-variable_loc);
-      variable_derivative_count = std::atoi(variable_derivative_count_str.c_str());
-      variable_loc = bracket_loc + 2; // "]["の長さ分
-      bracket_loc = variable_str.find("]", variable_loc);
-      if(bracket_loc == std::string::npos)
-      {
-        std::cout << "can't find bracket." << std::endl;
-//        return;
-      }
-      variable_loc += 6; // "usrVar"の長さ分
-      variable_name =  variable_str.substr(variable_loc, bracket_loc-variable_loc);
-    }
-    else
-    {
-      variable_name =  variable_str.substr(6); // "usrVar"の長さ分
-      variable_derivative_count = 0;
-    }
-
-    // 値の取得
-    int str_size = cons_str.size();
-    unsigned int end_loc = cons_str.rfind("]", str_size-1);
-
-    if(end_loc == std::string::npos)
-    {
-      std::cout << "can't find bracket." << std::endl;
-//      return;
-    }
-    std::string value_str = cons_str.substr(comma_loc + 1, end_loc - (comma_loc + 1));
-
-    SymbolicVariable symbolic_variable;
-    SymbolicValue symbolic_value;
-    symbolic_variable.name = variable_name;
-    symbolic_variable.derivative_count = variable_derivative_count;
-    symbolic_value.str = value_str;
-
-    variable_map.set_variable(symbolic_variable, symbolic_value); 
-    tmp_cons_it++;
-  }
-
-  NextPointPhaseState next_point_phase_state;
-  next_point_phase_state.next_point_phase_time = next_point_phase_time;
-  next_point_phase_state.variable_map = variable_map;
-  next_point_phase_state.is_max_time = is_max_time;
-  std::vector<NextPointPhaseState> next_point_phase_states_vector;
-  next_point_phase_states_vector.push_back(next_point_phase_state);
-*/
-  IntegrateResult integrate_result;
-  //integrate_result.states = next_point_phase_states_vector;
-  //integrate_result.ask_list = changed_asks;
 
   return integrate_result;
 }
