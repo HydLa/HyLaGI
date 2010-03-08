75b2d9fefcfb2f0c18f5b408ceaf78efe86d211f
hirose <hirose@db2ee5a7-f324-0410-8b40-dae30602bd36>
2010-03-08 12:32:23 +0000

    Windows用パッケージ化システム

--- create_win_package.bat
+++ create_win_package.bat
@@ -3,7 +3,7 @@ devenv HydLa.sln /Build Release
 for /F "tokens=1-3 delims=/ " %%a in ('date /t') do SET DT=HydLa_%%a%%b%%c
 md %DT%
 
-copy Release\HydLa_parser.exe %DT%\hydla.exe
+copy HydLa.exe %DT%\hydla.exe
 
 md %DT%\examples
 copy examples %DT%\examples
