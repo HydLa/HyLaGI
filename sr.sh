ca29e39d57ec266efcdf1649767cee438a46317c
yysaki <yysaki@db2ee5a7-f324-0410-8b40-dae30602bd36>
2012-01-08 06:58:47 +0000

    replace killall command in sr.sh

--- sr.sh
+++ sr.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+if [ -f reduce.pid ]
+then
+	# ‘¼‚ÌhydlaƒRƒ}ƒ“ƒh‚É‰“—¶‚µ‚È‚¢
+	kill `cat reduce.pid`
+	rm reduce.pid
+fi
 [REDUCE_PATH]/reduce -w -F- -L log_reducef.txt > /dev/null 2>&1 &
-./hydla $*
-killall reduce
+echo $! > reduce.pid
 
+./hydla $*
+
+kill `cat reduce.pid`
+rm reduce.pid
