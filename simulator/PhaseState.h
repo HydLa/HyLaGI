0b8d6abcf46335c319b2afee9adcaa4d11595d26
matsusho <matsusho@db2ee5a7-f324-0410-8b40-dae30602bd36>
2012-06-11 09:53:00 +0000

    add tag

--- simulator/PhaseState.h
+++ simulator/PhaseState.h
@@ -41,14 +41,17 @@ struct PhaseState {
   changed_asks_t            changed_asks;
   /// フェーズ内で一時的に追加する制約．分岐処理などに使用
   constraints_t temporary_constraints;
-  /// 実行アルゴリズムでのMSに相当
   module_set_container_sptr module_set_container;
+  /// 判定済みのモジュール集合を保持しておく．分岐処理時，同じ集合を複数回調べることが無いように
   std::set<module_set_sptr> visited_module_sets;
   /// シミュレーション実行ステップ数．IP を一度終えるごとに1増加する
   int step;
 
+  /// フェーズの終了状態．次のフェーズが無いときのみ意味を持つ
   CauseOfTermination cause_of_termination;
+  /// 次のフェーズ
   phase_state_sptrs_t children;
+  /// 前のフェーズ
   phase_state_sptr_t parent;
 };
 
