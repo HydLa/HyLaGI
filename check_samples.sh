e429cd56327c902fcc4c65a893532eaad960fea3
matsusho <matsusho@db2ee5a7-f324-0410-8b40-dae30602bd36>
2012-07-20 06:18:28 +0000

    improve script and split exsamples

--- check_samples.sh
+++ check_samples.sh
@@ -2,9 +2,9 @@
 
 no_good=0
 
-for file_name in ./samples/*.sample
-do
-    command_name=`head -n 1 $file_name`
+
+judge(){
+    command_name=`head -n 1 $1`
     command_result=`$command_name`
     sample=`tail -n +2 $file_name`
 #    echo $command_result
@@ -16,6 +16,16 @@ do
     else
         echo "$file_name OK"
     fi
+}
+
+for file_name in ./samples/*.sample
+do
+    judge $file_name
+done
+
+for file_name in ./samples_reduce/*.sample
+do
+    judge $file_name
 done
 
 if test $no_good -eq 0
--- make_sample.sh
+++ make_sample.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
+output(){
+    output_file=$1
+    shift
+    echo "./hyrose $@" > $output_file
+    ./hyrose $@ >> $output_file
+    cat $output_file
+}
 
-# 実行時に指定された引数の数、つまり変数 $# の値が 3 でなければエラー終了。
-if [ $# -lt 2 ]; then
-  echo "usege: $0 <output_file> <commands>..." 1>&2
+
+if [ $# -lt 1 ]; then
+  echo "usege: $0 <hydla_program> <options>..." 1>&2
   exit 1
 fi
 
-output_file=$1
+hydla=`echo $1 | sed -e  "s/.*\/\([^/]*\)\..*/\1/g"`
 
-shift
-
-echo "$@" > $output_file
-$@ >> $output_file
-cat $output_file
+output ./samples/$hydla.sample "$@"
+output ./samples_reduce/$hydla.sample "$@ -s r"
