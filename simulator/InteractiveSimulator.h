75e6b90ad86006146bc93c8af366d8756e5d3beb
wada <wada@db2ee5a7-f324-0410-8b40-dae30602bd36>
2012-10-03 08:42:00 +0000

    commit current interactive mode

--- simulator/InteractiveSimulator.h
+++ simulator/InteractiveSimulator.h
@@ -3,6 +3,9 @@
 
 #include "Simulator.h"
 #include "Logger.h"
+#include "version.h"
+//#include <ncurses.h>
+#include <curses.h>
 using namespace std;
 using namespace hydla::logger;
 
@@ -38,8 +41,7 @@ namespace hydla {
           /**
            * 与えられた解候補モジュール集合を元にシミュレーション実行をおこなう
            */
-          virtual void simulate()
-          {
+          virtual void simulate(){
             while(!state_stack_.empty()) {
               phase_result_sptr state(pop_phase_result());
               bool consistent;
@@ -47,42 +49,61 @@ namespace hydla {
               try{
                 if( Simulator<phase_result_t>::opts_->max_step >= 0 && state->step > Simulator<phase_result_t>::opts_->max_step)
                   continue;
-                if(state->phase == PointPhase)
-                {
-                  exit = interactivesimulate(state);
-                  //std::cout << get_state_output(*state,0,1) << std::endl;
-                  //std::cout << get_state_output(*phases[0]->parent,0,1) << std::endl;
-
-                }
+                all_state_.push_back(state); 
                 state->module_set_container->reset(state->visited_module_sets);
                 phase_result_sptrs_t phases = Simulator<phase_result_t>::phase_simulator_->simulate_phase(state, consistent);
+
+                if(state->phase == PointPhase){
+                  exit = interactivesimulate(state);
+                }
+
+                variable_map_t vm = phases[0]->parent->variable_map;
+                phase_result_t result = *phases[0]->parent;
+                vm = result.variable_map;
+                typename variable_map_t::const_iterator it  = vm.begin();
+                typename variable_map_t::const_iterator end = vm.end();
+
+                value_t wadavalue("(9, 10)");
+                //vm.set_variable(it->first,wadavalue);
+                std::cout << "++++++++--------" << std::endl;
+                for(; it!=end; ++it) {
+                  std::cout << *(it->first) << "\t: " << it->second << std::endl;
+                }
+                std::cout << "++++++++++-------"  << std::endl;
+                phases[0]->parent->variable_map = vm;
+
                 std::cout << get_state_output(*phases[0]->parent,0,1) << std::endl;
-
-
-                if(!phases.empty()){
-                  if(Simulator<phase_result_t>::opts_->nd_mode){
-                    for(typename phase_result_sptrs_t::iterator it = phases.begin();it != phases.end();it++){
-                      if(consistent){
-                        (*it)->module_set_container = Simulator<phase_result_t>::msc_no_init_;
-                      }
-                      else{
-                        (*it)->module_set_container = (*it)->parent->module_set_container;
-                      }
-                      push_phase_result(*it);
-                    }
-                  }else if(!phases.empty()){
-                    if(consistent){
-                      phases[0]->module_set_container = Simulator<phase_result_t>::msc_no_init_;
-                    }else{
-                      phases[0]->module_set_container = phases[0]->parent->module_set_container;
-                    }
-                    push_phase_result(phases[0]);
+                //std::cout << "size" << phases.size() << std::endl;
+                int selectcase = 0;
+                /* 変数変更ができるまでいったんコメントアウト できたら順番を考える
+                if(phases.size()>1){
+                  std::cout << "branch off into "<< phases.size() << "cases" << std::endl;
+                  std::cout << "-------------------------------------" << std::endl;
+                  for(int i=0;i<phases.size();i++){
+                    std::cout << "-----Case " << i << "--------------------------" << std::endl;
+                    std::cout << get_state_output(*phases[i]->parent,0,1) << std::endl;
+                  }
+                  std::cout << "-------------------------------------" << std::endl;
+                  std::cout << "-------------------------------------" << std::endl;
+                  while(1){
+                    std::cout << "input select case number" << std::endl;
+                    std::cin >> selectcase;
+                    if(!std::cin.fail())
+                      break;
+                    std::cin.clear();
+                    std::cin.ignore( 1024, '\n' );
                   }
                 }
-                if(exit)
-                {
-                  std::cout << get_state_output(*phases[0],0,1) << std::endl;
-                  //std::cout << get_state_output(state_stack_[0],0,1) << std::endl;
+                */
+                if(!phases.empty()){
+                  if(consistent){
+                    phases[selectcase]->module_set_container = Simulator<phase_result_t>::msc_no_init_;
+                  }else{
+                    phases[selectcase]->module_set_container = phases[selectcase]->parent->module_set_container;
+                  }
+                  push_phase_result(phases[selectcase]);
+                }
+                if(exit){
                   break;
                 }
               }catch(const std::runtime_error &se){
@@ -90,13 +111,6 @@ namespace hydla {
                 HYDLA_LOGGER_REST(se.what());
               }
             }
-            /*if(Simulator<phase_result_t>::opts_->output_format == fmtMathematica){
-              Simulator<phase_result_t>::output_result_tree_mathematica();
-              }
-              else{
-              Simulator<phase_result_t>::output_result_tree();
-              }
-              */
           }
 
           virtual void initialize(const parse_tree_sptr& parse_tree){
@@ -112,8 +126,6 @@ namespace hydla {
             state->parent = Simulator<phase_result_t>::result_root_;
             push_phase_result(state);
           }
-
-
           /**
            * 状態キューに新たな状態を追加する
            */
@@ -139,149 +151,111 @@ namespace hydla {
             return state;
           }
 
+          /*
+           * interactiveにsimulate
+           * 入力を受付けそれぞれの処理に飛ばす
+           * jを別メソッドに
+           */
+
           int interactivesimulate(phase_result_sptr& state){
-            all_state_.push_back(state); 
-            //std::cout << all_state_.size() << std::endl;
-            /* for(int i=0; i < all_state_.size() ; i++){
-               std::cout << get_state_output(*all_state_[i]->parent,0,1) << std::endl;
-               }*/
             //  change_variable_flag = false;
             Simulator<phase_result_t>::opts_->max_time = "100";
-            /*
-               if(0){
-               if(1){
-               char *name = "save_data.dat";
-               FILE *fp;
-               phase_result_sptr tmp;
-               if( ( fp = fopen( name, "rb" ) ) == NULL ) {
-               printf("ファイルオープンエラー\n");
-               }else{
-               while(fread( &*tmp, sizeof(368), 1, fp )){
-               std::cout << tmp->phase << std::endl;
-               all_state_.push_back(tmp);
-               }
-               }  
-               fclose( fp );
-               for (int i=all_state_.size();i<all_state_.size();i++){
-            //std::cout << get_state_output(*all_state_[i], false);
-            }
-            std::cout << "" << std::endl;
-            all_state_.clear();
-            }
-            }
-            if(all_state_.size() > 1){
-            for (int i=all_state_.size()-2;i<all_state_.size();i++){
-            //std::cout << get_state_output(*all_state_[i], false,true);
-            }
-            }else{
-            for (int i=0;i<all_state_.size();i++){
-            //std::cout << get_state_output(*all_state_[i], false,true);
-            }
-            }
-            */
-            std::cout << "[debug] in mode test" <<std::endl;
             std::string line;
             getline(cin,line);
 
             switch(line[0]){
               case 'j':
+                //jump
                 std::cout << "j" << std::endl;
-                /*
-                   int target_step;
-                   if(all_state_.size()!=0)
-                   {
-                //debug{{{
-                for(int i=0;i<all_state_.size();i++)
-                { 
-                if(all_state_[i]->phase==PointPhase)
+                int target_step;
+                if(all_state_.size()!=0)
                 {
-                cout <<  "[debug] step" << all_state_[i]->step+1 << " pp t:" << all_state_[i]-> current_time<< endl;
-                }else if(all_state_[i]->phase==IntervalPhase)
-                {
-                cout <<  "[debug] step" << all_state_[i]->step+1 << " ip t:" << all_state_[i]-> current_time<< "->" <<endl;
-                }
-                }
-                //debug}}}                          
-                cout << "in mode jump test input step number" <<endl;
-                cin >> target_step;
-                int i;
-                for(i=0;i<all_state_.size();i++)
-                {
-                if(all_state_[i]->step+1==target_step && all_state_[i]->phase==PointPhase)
-                {
-                cout << "jump to step "<<all_state_[i]->step+1 << " time:" << all_state_[i]->current_time << endl;
-                cout << "[debug] state size"<< all_state_.size() << "jump number" << i << endl;
-                state = all_state_[i];
-                int j=0;
-                int delete_size = all_state_.size()-i;
-                for(j=0;j<delete_size;j++)
-                {
-                all_state_.pop_back();
-                cout << "[debug] delete " << j << endl;
-                }
-                }
-                }
-                cin.ignore( 1024, '\n');
-                break;
+                  //debug{{{
+                  for(int i=0;i<all_state_.size();i++)
+                  { 
+                    if(all_state_[i]->phase==PointPhase)
+                    {
+                      cout <<  "[debug] step" << all_state_[i]->step+1 << " pp t:" << all_state_[i]-> current_time<< endl;
+                    }else if(all_state_[i]->phase==IntervalPhase)
+                    {
+                      cout <<  "[debug] step" << all_state_[i]->step+1 << " ip t:" << all_state_[i]-> current_time<< "->" <<endl;
+                    }
+                  }
+                  //debug}}}
+                  while(1){
+                    cout << "in mode jump test input step number" <<endl;
+                    std::cin >> target_step;
+                    if(!std::cin.fail())
+                      break;
+                    std::cin.clear();    // エラーをリセットします。
+                    std::cin.ignore(1024,'\n');    // 文字列を破棄します。
+                  }
+
+                  int i;
+                  for(i=0;i<all_state_.size();i++)
+                  {
+                    if(all_state_[i]->step+1==target_step && all_state_[i]->phase==PointPhase)
+                    {
+                      cout << "jump to step "<<all_state_[i]->step+1 << " time:" << all_state_[i]->current_time << endl;
+                      cout << "[debug] state size"<< all_state_.size() << "jump number" << i << endl;
+                      state = all_state_[i];
+                      int j=0;
+                      int delete_size = all_state_.size()-i;
+                      for(j=0;j<delete_size;j++)
+                      {
+                        all_state_.pop_back();
+                        cout << "[debug] delete " << j << endl;
+                      }
+                    }
+                  }
+                  cin.ignore( 1024, '\n');
+                  break;
                 }else
                 {
-                cout << "0 step" << endl;
-                break;
+                  cout << "0 step" << endl;
+                  break;
                 }
                 // default:
-                */
                 return 0;
-              case 's':{
-              //           save_state();
-                         /*
-                            cout << "save state in file" << endl;
-                            char *name = "save_data.dat"; // save_data.dat(セーブデータファイル)
-                            FILE *fp;
-                            phase_result_sptr tmp;
-                            if( ( fp = fopen( name, "wb" ) ) == NULL ) {
-                            printf("ファイルオープンエラー\n");
-                            }else{
-                            for(int i=0;i<all_state_.size();i++){
-                            tmp = all_state_[i];
-                            fwrite( &*tmp, sizeof(*tmp), 1, fp ) ;
-                            }
-                            }
-                            fclose( fp );
-                            */
+              case 's':
+                         //           save_state();
                          return 0;
-                       }
               case 'h':
-              //         show_helplist();
-                       //              cout << "help" << endl;
-                                     return 0;
+                       show_help();
+                       return 0;
               case 'w':
-                                     //change_variables();
-                       //            cout << "change variable" << endl;
+                       change_variables(state);
                        //change_variable_flag = 1;
                        return 0;
               case 'd':
                        select_options();
-                                return 0;
+                       return 0;
+              case 'b':
+                       set_breakpoint();
+              case 'p':
+                       print();
             }
-
-
             if(line[0] == 'q'||line[0] == 's'){
               cout << "exit" << endl;
               return 1;
             }
-
             return 0;
-
           }
 
+          /*
+           * hyroseのオプションを自由に変更する
+           * 現在debugオプションのみ
+           */
           int select_options(){
-            ProgramOptions &po = ProgramOptions::instance();
+            /*
+             * programoption がシングルトンなので対応策を考える 
+             ProgramOptions &po = ProgramOptions::instance();
             int argc = 0;
+            std::cout << "argc : " << argc << std::endl;
             char input[100], *argv[20], *cp;
             const char *delim = " \t\n"; 
             argv[0] = "./hyrose";
-            std::cout << "enter the hyrose option args (only debug options)" << std::endl;
-
+            std::cout << "enter the hyrose option args" << std::endl;
             if (fgets(input, sizeof(input), stdin) != NULL) {
               cp = input;
               for (argc = 1; argc < 20; argc++) {
@@ -291,9 +265,16 @@ namespace hydla {
               }
             }
             cp = NULL;
+            std::cout << "argc : " << argc << std::endl;
+            for (int i = 0; i < argc; i++) {
+              printf("%d番目の引数 = %s\n", i, argv[i]);
+            }
             po.parse(argc, argv);
             cp = NULL;
             std::string area_string(po.get<std::string>("debug"));
+            */
+            std::string area_string;
+            std::cin >> area_string;
             std::cout << "area_string : " << area_string << std::endl;
             if(area_string!=""){                 // デバッグ出力
               Logger::instance().set_log_level(Logger::Debug);
@@ -305,8 +286,15 @@ namespace hydla {
                 Logger::vcs_area_ = true;
                 Logger::extern_area_ = true;
                 Logger::rest_area_ = true;
-
-
+              }else if(area_string.find('n') != std::string::npos){
+                Logger::instance().set_log_level(Logger::None);
+                Logger::parsing_area_ = false;
+                Logger::calculate_closure_area_ = false;
+                Logger::phase_area_ = false;
+                Logger::vcs_area_ = false;
+                Logger::extern_area_ = false;
+                Logger::rest_area_ = false;
+                std::cout << "none" << std::endl;
               }else{
                 Logger::parsing_area_ = (area_string.find('p') != std::string::npos);
                 Logger::calculate_closure_area_ = (area_string.find('c') != std::string::npos);
@@ -319,8 +307,73 @@ namespace hydla {
             else {                              // 警告のみ出力
               Logger::instance().set_log_level(Logger::Warn);
             }
-            return 0; 
+            cin.ignore( 1024,'\n');
+            cin.clear();
+             return 0;
           }
+
+          /*
+           * interactiveモードにおけるヘルプを表示する
+           * 各コマンドの細かいhelp必要
+           */
+          int show_help(){
+            int ch;
+            const char *list[] = {
+              "breakpoints -- Making program stop at certain points",
+              "debug -- Simulate with debug-mode",
+              "edit -- Edit constraint ",
+              "help [command] -- Show help (for command)",
+              "jump --  Jump to certain step", 
+              "load -- Load state ",
+              "quit --  ",
+              "run -- simulate program  until the breakpoint is reached",
+              "save --  Save state to file",
+              "write -- Rewrite selected variable",
+              "print -- Display a variety of information"
+            };
+            initscr(); cbreak(); noecho();
+            while((ch = getch()) != 'q'){
+              mvaddstr(0, 0, "Push [q] to quit.");
+              mvaddstr(1, 0,"Type <return> to exec one step.");
+              mvaddstr(2, 0,"Type [command] <return> to exec command");
+              mvaddstr(4, 0,"List of commands:");
+              for(int i = 0;i<10;i++){
+                mvaddstr(i+5, 0, list[i]);
+              }
+            }
+            endwin();
+            return 0;
+          }
+          /*
+           * 変数の変更
+           * 区間値を入れる
+           */
+          int change_variables(phase_result_sptr& state){ 
+            std::cout << "change variable " << std::endl;
+            std::cout << "++++++++++++++++++++++++++++++++++++++++++++++++ " << std::endl;
+            variable_map_t vm = state->variable_map;
+            typename variable_map_t::const_iterator it  = vm.begin();
+            typename variable_map_t::const_iterator end = vm.end();
+            for(; it!=end; ++it) {
+              std::cout << *(it->first) << "\t: " << it->second << std::endl;
+            }
+            std::cout << "++++++++++++++++++++++++++++++++++++++++++++++++ " << std::endl;
+            return 0;
+          }
+          /*
+           * breakpoint をセットする
+           * 現状step実行のみなので、xが0になるまで実行とかできるように
+           */
+          int set_breakpoint(){
+            return 0;
+          }
+          /*
+           * printし直す
+           */
+          int print(){
+            return 0;
+          }
+
           /**
            * 各PhaseResultに振っていくID
            */
@@ -369,3 +422,4 @@ namespace hydla {
 } // hydla
 
 #endif // _INCLUDED_HYDLA_INTERACTIVE_SIMULATOR_H_
+
