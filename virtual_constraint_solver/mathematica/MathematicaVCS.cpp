2f1af76247abe773b7c08ba36dd5ae28ab4d2a95
wada <wada@db2ee5a7-f324-0410-8b40-dae30602bd36>
2012-10-10 08:41:12 +0000

    コミットミスを元に戻す

--- virtual_constraint_solver/mathematica/MathematicaVCS.cpp
+++ virtual_constraint_solver/mathematica/MathematicaVCS.cpp
@@ -301,7 +301,6 @@ MathematicaVCS::create_result_t MathematicaVCS::create_maps()
         throw SolveError("invalid value");
       }
       HYDLA_LOGGER_VCS("%% symbolic_value: ", symbolic_value);
-      //std::cout << tmp_range << std::endl;
       map.set_variable(variable_ptr, tmp_range);
     }
     create_result.result_maps.push_back(map);
