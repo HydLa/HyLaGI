e0c8bc9844fa9ac805ee85e0fc8ff0f56b58080f
matsusho <matsusho@db2ee5a7-f324-0410-8b40-dae30602bd36>
2012-11-06 10:13:00 +0000

    モジュール集合中のカンマで区切られてしまうので修正

--- output/CsvProfilePrinter.cpp
+++ output/CsvProfilePrinter.cpp
@@ -14,6 +14,7 @@ void CsvProfilePrinter::print_profile(const entire_profile_t& result) const
   cout << "#------Simulation Time------\n";
   if(result.size() > 0){
     cout << "Simulation Time, ";
+    std::set<std::string> label_set;
     for(unsigned int i = 0; i < result.size(); i++){
       phase_result_t& pr = *result[i]->phase_result;
       if(pr.phase == simulator::PointPhase)
@@ -25,12 +26,16 @@ void CsvProfilePrinter::print_profile(const entire_profile_t& result) const
         cout << "Interval";
       }
       cout << "Phase " << result[i]->phase_result->id << ", ";
+      for(profile_t::const_iterator it = result[i]->profile.begin(); it != result[i]->profile.end(); it++){
+        label_set.insert(it->first);
+        //フェーズによってシミュレーションしないモジュール集合などもあるため，必要なラベルをすべて列挙しておく．
+      }
     }
     cout << "\n";
-    for(profile_t::const_iterator it = result[0]->profile.begin(); it != result[0]->profile.end(); it++){ 
-      cout << it->first << ", ";
+    for(std::set<std::string>::const_iterator it = label_set.begin(); it != label_set.end(); it++){ 
+      cout << *it << ", ";
       for(unsigned int i = 0; i < result.size(); i++){
-        cout << result[i]->profile[it->first] << ", ";
+        cout << result[i]->profile[*it] << ", ";
       }
       cout << "\n";
     }
