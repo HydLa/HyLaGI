1685e940792b191ec4b5c20cb8e41508cf102abe
matsusho <matsusho@ueda.info.waseda.ac.jp>
2014-05-30 11:43:24 +0900

    use interval in calculateConsistentValue

--- src/backend/mathematica/math_source/approx.m
+++ src/backend/mathematica/math_source/approx.m
@@ -1,25 +1,56 @@
 (* calculate the value of given variable which satisfies given equation *)
 publicMethod[
   calculateConsistentValue,
-  equation, variable, variableMap, parameterMap,
+  equation, variable, variableMap, parameterCondition,
   Module[
-    {rules, sol, originalValue},
+    {rules, sol, originalValue, range, originalRange, intervalRules},
     (* ignore entry of variableMap for given variable *)
     rules = Select[variableMap, (#[[1]] =!= variable)&];
     rules = Map[(Rule@@#)&, rules];
     (* get entry of variableMap for given variable *)
     originalValue = Select[variableMap, (#[[1]] === variable)&];
-    originalValue = originalValue[[1]][[2]];
+    originalValue = originalValue[[1, 2]];
     simplePrint[rules, originalValue];
 
+    intervalRules = createIntervalRules[parameterCondition];
+  
     sol = Solve[equation //. rules, {variable}];
     simplePrint[sol];
-    sol = Select[sol, (Reduce[variable == #[[1]][[2]] && variable == originalValue && And@@parameterMap] =!= False)&];    
+    originalRange = N[originalValue /. intervalRules];
+    simplePrint[originalRange];
+    For[j = 1, j <= Length[sol], j++,
+      Print["interval:", N[sol[[j, 1, 2]] /. intervalRules]];
+    ];
+    sol = Select[sol, (IntervalIntersection[N[#[[1, 2]] /. intervalRules], originalRange] =!= Interval[])&];
     simplePrint[sol];
-    toReturnForm[sol[[1]][[1]][[2]] ]
+    If[Length[sol] == 1,
+      toReturnForm[sol[[1, 1, 2]] ],
+      Exception
+    ]
   ]
 ];
-   
+
+(* create rules to replace parameters with intervals of their ranges *)
+createIntervalRules[parameterCondition_] := 
+Module[
+  {intervalExpr, lbs, ubs, parameters, i, condition, parameter, rules = {} },
+  simplePrint[parameterCondition];
+  parameters = Union[Map[(#[[1]])&, parameterCondition] ];
+  simplePrint[parameters];
+  For[i = 1, i <= Length[parameterCondition], i++,
+    condition = parameterCondition[[i]];
+    parameter = condition[[1]];
+    If[Head[condition] === Less || Head[condition] === LessEqual, ubs[parameter] = condition[[2]] ];
+    If[Head[condition] === Greater || Head[condition] === GreaterEqual, lbs[parameter] = condition[[2]] ];
+    If[Head[condition] === Equal, ubs[parameter] = lbs[parameter] = condition[[2]] ]
+  ];
+  For[i = 1, i <= Length[parameters], i++,
+    parameter = parameters[[i]];
+    rules = Append[rules, parameter -> Interval[{lbs[parameter], ubs[parameter]}]]
+  ];
+  rules
+];
+
 
 publicMethod[
   intervalToMidpointRadius,
