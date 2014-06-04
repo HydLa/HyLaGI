9046fd5b99aa287b1fa3182e5a32babe9b21585b
matsusho <matsusho@ueda.info.waseda.ac.jp>
2014-06-04 12:21:34 +0900

    add definition of SignalHandler

--- src/common/SignalHandler.cpp
+++ src/common/SignalHandler.cpp
@@ -7,9 +7,15 @@ namespace signal_handler
 
 volatile sig_atomic_t interrupted = 0;
 
-void interrupt_handler(int sig);
+void interrupt_handler(int sig)
+{
+  interrupted = 1;
+}
 
-void term_handler(int sig);
+void term_handler(int sig)
+{
+  exit(-1);
+}
 
 
 }
