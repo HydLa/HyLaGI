5302b7a255e69473ef679c237f0e6c1359b0722b
matsusho <matsusho@ueda.info.waseda.ac.jp>
2014-09-05 17:48:22 +0900

    add sh for testing mathematica source codes on GUI

--- src/backend/mathematica/math_source/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-rlwrap math -run "optUseDebugPrint = True; timeOutS = Infinity; optIgnoreWarnings = True; SetOptions[\$Output, PageWidth->Infinity]; <<./load_first/common.m; <<./math_source.m << approx.m << static_analysis.m << util.m"
---
+++ test_math_source_gui.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+mathematica test_math_source.m
