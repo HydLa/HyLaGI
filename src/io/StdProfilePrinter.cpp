c2a51676b7c3ddb0d6ae8a7c75746d04d66b16e7
Fumihiko KOUNO <kouno@laguiole.ueda.info.waseda.ac.jp>
2015-02-09 18:27:44 +0900

    format std profile print

--- src/io/StdProfilePrinter.cpp
+++ src/io/StdProfilePrinter.cpp
@@ -11,12 +11,35 @@ namespace io{
 
 void StdProfilePrinter::print_profile(const entire_profile_t& result) const
 {
-  cout << "#------Simulation Time------\n";
+  int max_characters = 0;
+  int max_digits = 0;
+  std::string profile_title = "Simulation Time";
   for(auto phase : result)
   {
     for(auto entry : phase->profile){
-      cout << entry.first << "\t: " << entry.second << endl;
+      if(max_characters < entry.first.length()) max_characters = entry.first.length();
+      if(max_digits < std::to_string(entry.second).length()) max_digits = std::to_string(entry.second).length();
     }
+  }
+  cout << "#";
+  for(int i = 0; i < (max_characters + max_digits + 2 - profile_title.length()) / 2; i++) cout << "=";
+  cout << profile_title;
+  for(int i = 0; i < (max_characters + max_digits + 2 - profile_title.length()) / 2; i++) cout << "=";
+  cout << endl << endl;
+  for(auto phase : result)
+  {
+    for(int i = 0; i < (max_characters + max_digits - 3) / 2; i++) cout << "-";
+    cout << " ";
+    cout << (phase->phase_type==simulator::POINT_PHASE ? "PP " : "IP ") << phase->id;
+    cout << " ";
+    for(int i = 0; i < (max_characters + max_digits - 3) / 2; i++) cout << "-";
+    cout << endl;
+    for(auto entry : phase->profile){
+      cout << entry.first; 
+      for(int i = entry.first.length(); i < max_characters; i++) cout << " "; 
+      cout << " : " << entry.second << endl;
+    }
+    cout << endl;
   }
 }
 
