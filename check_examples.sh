4b9b10171172f7ac1b66f0470c06d54a26bcb4f1
matsusho <matsusho@ueda.info.waseda.ac.jp>
2015-05-12 13:50:03 +0900

    add check_examples/sh and .gitignore

---
+++ check_examples.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+find ./examples/ \( -type f -and -name '*.hydla' \) | while read FILE
+do
+    echo ${FILE}
+    ./bin/hylagi ${FILE}
+    if $? != 0; then
+        echo "Something is wrong!"
+        break
+    fi
+done
+
