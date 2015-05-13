dead9370a95142c6b7555c1b7af615a92c5e91f7
matsusho <matsusho@ueda.info.waseda.ac.jp>
2015-05-13 16:56:22 +0900

    move and modify scripts

--- scripts/find_all.sh
+++ scripts/find_all.sh
@@ -3,5 +3,5 @@
 if test $# -ne 1 ; then 
   echo "usage: find_all.sh [word_to_find]"
 else
-  find .. \( -type d -and -name '.git' -and -prune \) -or \( -type f -and \( -name \*.cpp -or -name \*.h \) -and -print \) | xargs grep $1
+  find . \( -type d -and -name '.git' -and -prune \) -or \( -type f -and \( -name \*.cpp -or -name \*.h \) -and -print \) | xargs grep $1
 fi
