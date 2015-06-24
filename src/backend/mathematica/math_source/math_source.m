0d08574e93bec7c26cf570a878dcbf44430c8259
matsusho <matsusho@ueda.info.waseda.ac.jp>
2015-06-24 16:41:57 +0900

    fix circle.hydla and message of exDSolve::multi

--- src/backend/mathematica/math_source/math_source.m
+++ src/backend/mathematica/math_source/math_source.m
@@ -674,7 +674,7 @@ Module[
         unsolvable = True;
         Continue[]
       ];
-      If[Length[rules] > 1, Message[exDSolve::multi, expr];checkMessage ];
+      If[Length[rules] > 1, Message[exDSolve::multi, searchResult[[1]]];checkMessage ];
       resultRule = Union[resultRule, rules[[1]] ];
        listExpr = applyDSolveResult[searchResult[[2]], rules[[1]] ];
       If[MemberQ[listExpr, ele /; (ele === False || (!hasVariable[ele] && MemberQ[ele, t, Infinity]))], Return[overConstraint] ];
