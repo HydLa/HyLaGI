bb06933735ab11529c466877507c948b44bb6227
matsusho <matsusho@ueda.info.waseda.ac.jp>
2015-12-01 17:17:44 +0900

    fix pattern matching in exDSolve

--- src/backend/mathematica/math_source/math_source.m
+++ src/backend/mathematica/math_source/math_source.m
@@ -167,7 +167,6 @@ publicMethod[
     {sol, tStore, ret},
     sol = exDSolve[cons, prevRs];
     sol = sol /. prevRs;
-    simplePrint["prevRs@cvminterval: ", prevRs];
     debugPrint["sol after exDSolve", sol];
     If[sol === overConstrained || sol[[1]] === underConstrained,
       Message[createVariableMapInterval::underconstrained],
@@ -707,7 +706,7 @@ Module[
       resultRule = Union[resultRule, rules[[1]] ];
       listExpr = applyDSolveResult[searchResult[[2]], rules[[1]] ];
       listExpr = listExpr //. prevRs;
-      If[MemberQ[listExpr, ele /; (ele === False || (!hasVariable[ele] && MemberQ[ele, t, Infinity]))], Return[overConstrained] ];
+      If[MemberQ[listExpr, ele_ /; (ele === False || (!hasVariable[ele] && MemberQ[ele, t, Infinity]))], Return[overConstrained] ];
       listExpr = Select[listExpr, (#=!=True)&];
       tVars = Map[(#[t])&, getVariablesWithDerivatives[listExpr] ];
       simplePrint[listExpr, tVars];
