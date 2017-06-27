9cc239052c2cfd15332fcd431ba1c973b50b6395
Kenichi Betsuno <betsuno@ueda.info.waseda.ac.jp>
2017-06-27 18:14:07 +0900

    use std::vector as a container in std::stack

--- src/interval/IntervalNewton.h
+++ src/interval/IntervalNewton.h
@@ -18,7 +18,7 @@ namespace interval
 typedef kv::dd dd;
 typedef kv::interval<double> itvd;
 // typedef kv::interval<dd> itvd;
-typedef std::stack<itvd> itv_stack_t;
+typedef std::stack<itvd, std::vector<itvd>> itv_stack_t;
 
 typedef hydla::symbolic_expression::node_sptr node_sptr;
 typedef simulator::parameter_map_t            parameter_map_t;
