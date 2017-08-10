e23186ac937300429da4e81c2764b15b1c9f4405
agehama <agehama1@gmail.com>
2017-08-10 15:56:41 +0900

    refactor Logger.h(BOOST_PP_* -> variadic templates)

--- src/common/Logger.h
+++ src/common/Logger.h
@@ -1,48 +1,12 @@
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <sstream>
 #include <boost/iostreams/filtering_stream.hpp>
 
 namespace hydla {
 namespace logger {
-
-/**
- * ログ出力関数において対応する引数の数
- */
-#define HYDLA_LOGGER_LOG_WRITE_ARG_NUM 20
-
-/**
- * ログ出力関数内の定義
- */
-#define HYDLA_LOGGER_LEFT_SHIFT_ARGS_GEN(z, n, d) \
-  << BOOST_PP_CAT(a, n)
-
-/**
- * 指定された値の数の引数をもつログ出力関数を生成
- */
-
-//dの要素 => (関数名, フィルタ変数)
-#define HYDLA_LOGGER_DEF_LOG_WRITE_GEN(z, n, d)                   \
-  template< BOOST_PP_ENUM_PARAMS(n, typename A) >                 \
-  void BOOST_PP_TUPLE_ELEM(2, 0, d)(                              \
-    BOOST_PP_ENUM_BINARY_PARAMS(n, const A, &a) ) {               \
-    BOOST_PP_TUPLE_ELEM(2, 1, d)                                  \
-      BOOST_PP_REPEAT(n, HYDLA_LOGGER_LEFT_SHIFT_ARGS_GEN, _)     \
-      << std::endl;                                               \
-  }
-
-
-/**
- * 指定されたレベルのログ出力関数を生成
- */
-#define HYDLA_LOGGER_DEF_LOG_WRITE(FUNC, LEVEL, FILTER) \
-  BOOST_PP_REPEAT_FROM_TO(                              \
-    1,                                                  \
-    BOOST_PP_INC(HYDLA_LOGGER_LOG_WRITE_ARG_NUM),       \
-    HYDLA_LOGGER_DEF_LOG_WRITE_GEN,                     \
-    (FUNC, FILTER))
-
-
 
 class Logger
 {
@@ -80,22 +44,54 @@ public:
   /**
    * ログレベルdebugとしてログの出力をおこなう
    */
-  HYDLA_LOGGER_DEF_LOG_WRITE(debug_write, Debug, debug_)
+  template<typename... As>
+  static void debug_write(const As&... args) {
+    hydla::logger::Logger& i = hydla::logger::Logger::instance();
+    std::ostringstream stream;
+    using List = int[];
+    List{ ((stream << args), 0) ... };
+
+    i.debug_ << stream.str() << std::endl;
+  }
 
   /**
    * ログレベルwarnとしてログの出力をおこなう
    */
-  HYDLA_LOGGER_DEF_LOG_WRITE(warn_write,  Warn,  warn_)
+  template<typename... As>
+  static void warn_write(const As&... args) {
+    hydla::logger::Logger& i = hydla::logger::Logger::instance();
+    std::ostringstream stream;
+    using List = int[];
+    List{ ((stream << args), 0) ... };
+
+    i.warn_ << stream.str() << std::endl;
+  }
 
   /**
    * ログレベルerrorとしてログの出力をおこなう
    */
-  HYDLA_LOGGER_DEF_LOG_WRITE(error_write, Error, error_)
+  template<typename... As>
+  static void error_write(const As&... args) {
+    hydla::logger::Logger& i = hydla::logger::Logger::instance();
+    std::ostringstream stream;
+    using List = int[];
+    List{ ((stream << args), 0) ... };
+
+    i.error_ << stream.str() << std::endl;
+  }
   
   /**
    * ログレベルfatalとしてログの出力をおこなう
    */
-  HYDLA_LOGGER_DEF_LOG_WRITE(fatal_write, Fatal, fatal_)
+  template<typename... As>
+  static void fatal_write(const As&... args) {
+    hydla::logger::Logger& i = hydla::logger::Logger::instance();
+    std::ostringstream stream;
+    using List = int[];
+    List{ ((stream << args), 0) ... };
+
+    i.fatal_ << stream.str() << std::endl;
+  }
 
 private:
   Logger();
@@ -103,6 +99,7 @@ private:
   Logger& operator=(const Logger&); 
 
   LogLevel log_level_;
+  bool is_html_mode = false;
 
   boost::iostreams::filtering_ostream debug_;
   boost::iostreams::filtering_ostream warn_;
@@ -110,17 +107,7 @@ private:
   boost::iostreams::filtering_ostream fatal_;
 };  
 
-
-#define HYDLA_LOGGER_LOG_WRITE_MACRO(LEVEL, FUNC, ARGS) {             \
-    hydla::logger::Logger& logger=hydla::logger::Logger::instance();  \
-    if(logger.valid_level(hydla::logger::Logger::LEVEL)) {            \
-      logger. FUNC ARGS;                                              \
-    }                                                                 \
-  }
-
-
-#define HYDLA_LOGGER_DEBUG_INTERNAL(...)                                    \
-  HYDLA_LOGGER_LOG_WRITE_MACRO(Debug, debug_write, (__VA_ARGS__))
+#define HYDLA_LOGGER_DEBUG_INTERNAL(...)  hydla::logger::Logger::debug_write(__VA_ARGS__)
 
 /**
  * ログレベルdebugでのログの出力
@@ -129,35 +116,27 @@ private:
 #define HYDLA_LOGGER_DEBUG(...)                                  \
   HYDLA_LOGGER_DEBUG_INTERNAL("@", __FILE__, " ", __LINE__, " function: ", __FUNCTION__, "   ",  __VA_ARGS__)
 
-
 /**
  * ログレベルwarnでのログの出力
  */
+#define HYDLA_LOGGER_WARN_INTERNAL(...)  hydla::logger::Logger::warn_write(__VA_ARGS__)
 
-#define HYDLA_LOGGER_WARN_INTERNAL(...)                                    \
-  HYDLA_LOGGER_LOG_WRITE_MACRO(Warn, warn_write, (__VA_ARGS__))
-
-#define HYDLA_LOGGER_WARN(...)                                  \
-  HYDLA_LOGGER_WARN_INTERNAL("WARNING: ", __VA_ARGS__)
+#define HYDLA_LOGGER_WARN(...)  HYDLA_LOGGER_WARN_INTERNAL("WARNING: ", __VA_ARGS__)
 
 /**
  * ログレベルerrorでのログの出力
  */
-#define HYDLA_LOGGER_ERROR(...)                                   \
-  HYDLA_LOGGER_LOG_WRITE_MACRO(Error, error_write, (__VA_ARGS__))
+#define HYDLA_LOGGER_ERROR(...)  hydla::logger::Logger::error_write(__VA_ARGS__)
 
 /**
  * ログレベルfatalでのログの出力
  */
-#define HYDLA_LOGGER_FATAL(...)                                   \
-  HYDLA_LOGGER_LOG_WRITE_MACRO(Fatal, fatal_write, (__VA_ARGS__))
+#define HYDLA_LOGGER_FATAL(...)  hydla::logger::Logger::fatal_write(__VA_ARGS__)
 
 /**
  * log macro for variables (printed like "(name of variable): (value of var)")
  */
-#define HYDLA_LOGGER_DEBUG_VAR(VAR)                                            \
-  HYDLA_LOGGER_DEBUG(#VAR": ", VAR)
-
+#define HYDLA_LOGGER_DEBUG_VAR(VAR)  HYDLA_LOGGER_DEBUG(#VAR": ", VAR)
 
 } // namespace logger
 } // namespace hydla
