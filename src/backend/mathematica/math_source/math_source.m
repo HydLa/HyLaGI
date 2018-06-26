21e79d7f3a74dcae652502cea270d1e036e68d2f
masa-310 <masashi@ueda.info.waseda.ac.jp>
2018-06-26 21:52:44 +0900

    fix mistake

--- src/backend/mathematica/math_source/math_source.m
+++ src/backend/mathematica/math_source/math_source.m
@@ -726,8 +726,8 @@ publicMethod[
         {},
         ret = makeListFromPiecewise[minT, necessaryPCons];
         ret = Map[({#[[1]], #[[2]] && restPCons})&, ret];
-+       (* 時刻が0となる場合は弾く．*)
-+       ret = Select[ret, (#[[1]] =!= 0)&];
+        (* 時刻が0となる場合は弾く．*)
+        ret = Select[ret, (#[[1]] =!= 0)&];
         ret = Select[ret, (#[[2]] =!= False)&];
 
         (* 整形して結果を返す *)
