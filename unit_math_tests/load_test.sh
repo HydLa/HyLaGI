db1ee1a0d838d1a2ebdc921855d078cb61f0d852
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2019-11-26 14:28:43 +0900

    fix load_test

--- unit_math_tests/load_test.sh
+++ unit_math_tests/load_test.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-cd ../
-timeout -s SIGKILL 1.5 ./scripts/test_math_source.sh &> log
-stty sane
-ln=$(grep -c '' log)
-rm -f log
-if [ $ln -ne 4 ] ; then
+if [ $(./load.m | grep -c '') -ne 0 ] ; then
   echo "load_math_source failed..."
   exit 1
 fi
