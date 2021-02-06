69e45140e7ead34b756ac2bc1ae77b8312ac1bcf
YunosukeY <yunosuke@ueda.info.waseda.ac.jp>
2021-02-06 22:54:47 +0900

    remove unused scripts

--- scripts/find_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if test $# -ne 1 ; then 
-  echo "usage: find_all.sh [word_to_find]"
-else
-  find . \( -type d -and -name '.git' -and -prune \) -or \( -type f -and \( -name \*.cpp -or -name \*.h -or -name \*.m \) -and -print \) | xargs grep $1
-fi
