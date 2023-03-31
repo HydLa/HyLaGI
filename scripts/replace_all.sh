674af17cef3f71be86e292b1ce144b402595e70b
khata1007 <hata@ueda.info.waseda.ac.jp>
2023-03-31 15:51:03 +0900

    remove unnecessary files

--- scripts/replace_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if test $# -ne 2 ; then 
-  echo "usage: replace_all.sh [before] [after]"
-else
-  find . \( -type d -and -name '.git' -and -prune \) -or \( -type f -and \( -name \*.cpp -or -name \*.h  -or -name \*.m \) -and -print \) | xargs sed -i "s/${1}/${2}/g"
-fi
